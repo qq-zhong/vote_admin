2346b3f037b9c0e5c6f323b1e1b5c5bf9fd87020
qq-zhong <peter.qq.zhong@gmail.com>
2025-05-01 19:57:10 -0700

    local

--- src/App.tsx
+++ src/App.tsx
@@ -33,20 +33,14 @@ function App() {
         console.log("old cookie is : ", document.cookie)
         console.log("App component rendered");
         
-<<<<<<< HEAD
+        // 
         // const serverAddress = "wss://peterzhong.ca:2096?role=admin"
         const serverAddress = "wss://209.121.228.173:2096?role=admin"
         // const socket = new WebSocket(serverAddress);
         const cookie = encodeURIComponent(document.cookie);
         // const socket = new WebSocket(`wss://peterzhong.ca:2096?role=admin&cookie=${cookie}`);
         const socket = new WebSocket(`wss://209.121.228.173:2096?role=admin&cookie=${cookie}`);
-=======
-        
-        // const socket = new WebSocket(serverAddress);
-        const cookie = encodeURIComponent(document.cookie);
-        const socket = new WebSocket(`wss://peterzhong.ca:2096?role=admin&cookie=${cookie}`);
         const reconnectString = `wss://peterzhong.ca:2096?role=admin&cookie=${cookie}`;
->>>>>>> 75be12fbefb0a288afc4b5868b7136abd7098fe1
 
         let userID = getCookie("userID");
 
