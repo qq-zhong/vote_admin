8a721ef133111f17fe8f837e5512c1c3f114602e
qq-zhong <peter.qq.zhong@gmail.com>
2025-05-01 19:57:53 -0700

    ip fix

--- src/App.tsx
+++ src/App.tsx
@@ -34,12 +34,10 @@ function App() {
         console.log("App component rendered");
         
         // 
-        // const serverAddress = "wss://peterzhong.ca:2096?role=admin"
-        const serverAddress = "wss://209.121.228.173:2096?role=admin"
+        const serverAddress = "wss://peterzhong.ca:2096?role=admin"
         // const socket = new WebSocket(serverAddress);
         const cookie = encodeURIComponent(document.cookie);
-        // const socket = new WebSocket(`wss://peterzhong.ca:2096?role=admin&cookie=${cookie}`);
-        const socket = new WebSocket(`wss://209.121.228.173:2096?role=admin&cookie=${cookie}`);
+        const socket = new WebSocket(`wss://peterzhong.ca:2096?role=admin&cookie=${cookie}`);
         const reconnectString = `wss://peterzhong.ca:2096?role=admin&cookie=${cookie}`;
 
         let userID = getCookie("userID");
